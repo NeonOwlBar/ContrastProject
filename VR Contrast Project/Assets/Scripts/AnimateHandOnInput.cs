ee10539e0ca1d728566d84956127a2ee11569d14
Rowan Noble <rowan.noble@uwclub.net>
2024-04-06 13:47:05 +0100

    Poke Input Only On Point, Image Names Adjusted
    The AnimateHandOnInput class has been adjusted so that if either the grip or trigger value changes, these are checked to see if trigger == 0 and grip == 1, which represents a point animation. If so, set the poke object to active, else deactivate it. This means the menu can only be interacted with when pointing, which helps to prevent unwanted adjustments.
    
    Skybox names have been adjusted so the numbering is always two digits. This will help in future when data is collected to know which skybox is activated by a user.

--- VR Contrast Project/Assets/Scripts/AnimateHandOnInput.cs
+++ VR Contrast Project/Assets/Scripts/AnimateHandOnInput.cs
@@ -7,9 +7,13 @@ public class AnimateHandOnInput : MonoBehaviour
 {
     public InputActionProperty pinchAnimationAction;
     public InputActionProperty gripAnimationAction;
+    public GameObject pokeObject;
     public Animator handAnimator;
     private float prevTrigger;
     private float prevGrip;
+
+    private bool triggerChanged;
+    private bool gripChanged;
 
     // Update is called once per frame
     void Update()
@@ -19,6 +23,7 @@ public class AnimateHandOnInput : MonoBehaviour
         {
             handAnimator.SetFloat("Trigger", triggerValue);
             prevTrigger = triggerValue;
+            triggerChanged = true;
         }
         //Debug.Log(triggerValue);
 
@@ -27,6 +32,24 @@ public class AnimateHandOnInput : MonoBehaviour
         {
             handAnimator.SetFloat("Grip", gripValue);
             prevGrip = gripValue;
+            gripChanged = true;
+        }
+
+        // two separate statements so only reset when necessary
+        if (triggerChanged || gripChanged)
+        {
+            if (triggerValue == 0 && gripValue == 1 && !pokeObject.activeInHierarchy)
+            {
+                pokeObject.SetActive(true);
+            }
+            else if (pokeObject.activeInHierarchy)
+            {
+                pokeObject.SetActive(false);
+            }
+
+            // reset bools
+            gripChanged = false;
+            triggerChanged = false;
         }
     }
 }
